629caa053be9dbebc559cae35f90fc32c4cbd543
Gairik-Shandilya <104648687+Gairik-Shandilya@users.noreply.github.com>
2023-03-20 23:35:11 +0530

    One day Anna got the.c

--- Structure Pointers and Array Pointers/One day Anna got the.c
+++ Structure Pointers and Array Pointers/One day Anna got the.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 #include<stdlib.h>
-int cmp(const void *a,const void *b)
-{
+int cmp(const void *a,const void *b){
     return (*(int*)a -*(int*)b);
 }
-int main()
-{ int N,i;
-  scanf("%d",&N);
-  int *aa=(int*)malloc(N*sizeof(int));
-  for(i=0;i<N;i++)
-   scanf("%d",aa+i);
-  qsort(aa,N,sizeof(int),cmp);
-  N--;
-  if((aa[N]-aa[0])>2)
-   printf("NO");
-  else
-   printf("YES");
-  return 0;
+int main(){
+    int N,i;
+    scanf("%d",&N);
+    int *aa=(int*)malloc(N*sizeof(int));
+    for(i=0;i<N;i++)
+        scanf("%d",aa+i);
+    qsort(aa,N,sizeof(int),cmp);
+    N--;
+    if((aa[N]-aa[0])>2)
+        printf("NO");
+    else
+        printf("YES");
+    return 0;
 }
